4a83903a67e708632b957df73905b1e7469e49c1
ertugrulungor <ertugrulungor@gmail.com>
2016-10-20 17:11:59 +0300

    First blood

---
+++ FareAlgoritmasi/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FareAlgoritmasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnCiz = new System.Windows.Forms.Button();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.pnlIcerik = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBasla);
+            this.panel1.Controls.Add(this.btnCiz);
+            this.panel1.Controls.Add(this.txtY);
+            this.panel1.Controls.Add(this.txtX);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(789, 0);
+            this.panel1.MaximumSize = new System.Drawing.Size(132, 0);
+            this.panel1.MinimumSize = new System.Drawing.Size(132, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(132, 557);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(13, 67);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(106, 23);
+            this.btnBasla.TabIndex = 7;
+            this.btnBasla.Tag = "-1,-1";
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnCiz
+            // 
+            this.btnCiz.Location = new System.Drawing.Point(13, 38);
+            this.btnCiz.Name = "btnCiz";
+            this.btnCiz.Size = new System.Drawing.Size(106, 23);
+            this.btnCiz.TabIndex = 6;
+            this.btnCiz.Tag = "-1,-1";
+            this.btnCiz.Text = "Çiz";
+            this.btnCiz.UseVisualStyleBackColor = true;
+            this.btnCiz.Click += new System.EventHandler(this.btnCiz_Click);
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(69, 12);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(50, 20);
+            this.txtY.TabIndex = 5;
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(13, 12);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(50, 20);
+            this.txtX.TabIndex = 4;
+            // 
+            // pnlIcerik
+            // 
+            this.pnlIcerik.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlIcerik.Location = new System.Drawing.Point(0, 0);
+            this.pnlIcerik.Name = "pnlIcerik";
+            this.pnlIcerik.Size = new System.Drawing.Size(789, 557);
+            this.pnlIcerik.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 557);
+            this.Controls.Add(this.pnlIcerik);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnCiz;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Panel pnlIcerik;
+    }
+}
+
---
+++ FareAlgoritmasi/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FareAlgoritmasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[,] dizi;
+        int xMax, yMax;
+
+        private void btnCiz_Click(object sender, EventArgs e)
+        {
+            int deger1, deger2;
+            bool deger1Kontrol = true, deger2Kontrol = true;
+            foreach (char item in txtX.Text)
+            {
+                if (item < 47 || item > 58)
+                {
+                    deger1Kontrol = false;
+                    break;
+                }
+            }
+
+            foreach (char item in txtY.Text)
+            {
+                if (item < 47 || item > 58)
+                {
+                    deger2Kontrol = false;
+                    break;
+                }
+            }
+            if(deger1Kontrol && deger2Kontrol && txtX.Text != "" && txtY.Text != "")
+            {
+                deger1 = Convert.ToInt32(txtX.Text);
+                deger2 = Convert.ToInt32(txtY.Text);
+                dizi = new int[deger1, deger2];
+                for (int i = 0; i < deger1; i++)
+                {
+                    for (int j = 0; j < deger2; j++)
+                    {
+                        dizi[i, j] = 0;
+                    }
+                }
+                xMax = deger1;
+                yMax = deger2;
+                Ciz(deger1, deger2);
+            }
+            else MessageBox.Show("Lütfen sayı giriniz");
+
+        }
+
+        private void btnDuvar_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            string[] bol = new string[2];
+            bol = b.Tag.ToString().Split(',');
+            b.Image = Properties.Resources.duvar;
+            b.Text = "";
+            dizi[Convert.ToInt32(bol[0]), Convert.ToInt32(bol[1])] = -1;
+        }
+
+        private async void btnBasla_Click(object sender, EventArgs e)
+        {
+            txtX.Enabled = false;
+            txtY.Enabled = false;
+            btnCiz.Enabled = false;
+            pnlIcerik.Enabled = false;
+            btnBasla.Enabled = false;
+            await hareket();
+        }
+
+        private async Task<int> hareket()
+        {
+            int x = 1, y = 1;
+            while (true)
+            {
+                if (x > xMax || y > yMax) break;
+                int xBulunan = 0, yBulunan = y, min = 0, bulunan = 0;
+
+                int ust = y - 1;
+                int alt = y + 1;
+                int sol = x - 1;
+                int sag = x + 1;
+                int[] siralama = new int[] { int.MaxValue, dizi[x, ust], dizi[x, alt], dizi[sol, y], dizi[sag, y] };
+                for (int i = 1; i < siralama.Length; i++)
+                {
+                    if (siralama[i] < siralama[min] && siralama[i] != -1)
+                    {
+                        min = i;
+                        if (i == 1)
+                        {
+                            xBulunan = x;
+                            yBulunan = ust;
+                        }
+                        else if (i == 2)
+                        {
+                            xBulunan = x;
+                            yBulunan = alt;
+                        }
+                        else if (i == 3)
+                        {
+                            xBulunan = sol;
+                            yBulunan = y;
+                        }
+                        else if (i == 4)
+                        {
+                            xBulunan = sag;
+                            yBulunan = y;
+                        }
+                        bulunan = i;
+                    }
+                }
+
+                foreach (var i in pnlIcerik.Controls)
+                {
+                    if (i is Button)
+                    {
+                        string[] bol = new string[2];
+                        bol = (i as Button).Tag.ToString().Split(',');
+                        if (bol[0] == xBulunan.ToString() && bol[1] == yBulunan.ToString())
+                        {
+                            (i as Button).Image = Properties.Resources.mouse;
+                            (i as Button).Text = "";
+                            await Task.Delay(150);
+                            dizi[xBulunan, yBulunan] += 1;
+                            (i as Button).Text = dizi[xBulunan, yBulunan].ToString();
+                            (i as Button).BackColor = Color.Transparent;
+                            (i as Button).Image = null;
+                            break;
+                        }
+                    }
+                }
+                x = xBulunan;
+                y = yBulunan;
+
+                await Task.Delay(150);
+            }
+            return 0;
+        }
+
+        private void Ciz(int x, int y)
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (i == 0)
+                        dizi[i, j] = -1;
+                    else if (j == 0)
+                        dizi[i, j] = -1;
+                    else if (j == y - 1)
+                        dizi[i, j] = -1;
+                    else if (i == x - 1)
+                        dizi[i, j] = -1;
+                }
+            }
+
+            int xPoint = 0, yPoint = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Button b = new Button();
+                    if (dizi[i, j] == -1)
+                    {
+                        b.Image = Properties.Resources.duvar;
+                    }
+                    else
+                    {
+                        b.Text = "0";
+                        b.BackColor = Color.Transparent;
+                    }
+
+                    b.FlatStyle = FlatStyle.Flat;
+                    b.Top = yPoint;
+                    b.Left = xPoint;
+                    b.Height = 50;
+                    b.Width = 50;
+                    b.Tag = i + "," + j;
+                    b.Click += btnDuvar_Click;
+                    pnlIcerik.Controls.Add(b);
+
+                    if (j == y - 1)
+                    {
+                        xPoint = 0;
+                        yPoint += 50;
+                    }
+                    else
+                    {
+                        xPoint += 50;
+                    }
+                }
+            }
+        }
+    }
+}
